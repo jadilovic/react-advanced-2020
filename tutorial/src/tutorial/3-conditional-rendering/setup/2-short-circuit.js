ffa1876ed6f4fc9da07de71f67be2bd6424f38f8
jadilovic <adilovic79@yahoo.com>
2021-04-06 21:31:04 +0200

    show hide

--- tutorial/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ tutorial/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
 
-  return <h2>short circuit</h2>;
+  // const firstValue = text || "hello world";
+  // const secondValue = text && "hello world";
+
+  return (
+    <div>
+      {
+        // <h1>{firstValue}</h1>
+        // <h2>value: {secondValue}</h2>
+      }
+      <h2>{text || "john doe"}</h2>
+      <h2>{!text && "tom jerry"}</h2>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        toggle button
+      </button>
+      <h2>{isError && "Error tom jerry"}</h2>
+      {isError ? (
+        <p>there is an error...</p>
+      ) : (
+        <div>
+          <h2>there is no no no error</h2>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ShortCircuit;
--- tutorial/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ tutorial/src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,38 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  const [show, setShow] = useState(false);
+
+  return (
+    <div>
+      <button className="btn" onClick={() => setShow(!show)}>
+        show/hide
+      </button>
+      {show && <Item />}
+    </div>
+  );
+};
+
+const Item = () => {
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkSize);
+    return () => {
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+
+  return (
+    <div style={{ marginTop: "2rem" }}>
+      <h2>window</h2>
+      <h2>value: {size}</h2>
+    </div>
+  );
 };
 
 export default ShowHide;
