7d9360fb5cc272dc2d8fea4cd6280dcd39a473d3
jadilovic <adilovic79@yahoo.com>
2021-04-06 10:02:24 +0200

    use effect

--- birthday/src/List.js
+++ birthday/src/List.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 const List = ({ people }) => {
+  const [listPeople, setListPeople] = useState(people);
+  const removeItem = (id) => {
+    const newList = listPeople.filter((person) => {
+      return person.id !== id;
+    });
+    setListPeople(newList);
+  };
   return (
     <>
-      {people.map((person) => {
+      {listPeople.map((person) => {
         const { id, name, age, image } = person;
         return (
           <article key={id} className="person">
@@ -12,6 +19,9 @@ const List = ({ people }) => {
               <h4>{name}</h4>
               <p>{age}</p>
             </div>
+            <button className="btn" onClick={() => removeItem(id)}>
+              remove birthday
+            </button>
           </article>
         );
       })}
--- tutorial/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ tutorial/src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    console.log("use effect");
+    if (value > 0) {
+      document.title = `New Message ${value}`;
+    }
+  }, [value]);
+
+  useEffect(() => {
+    console.log("hellow world");
+  }, []);
+
+  console.log("render component");
+  return (
+    <div>
+      <h1>{value}</h1>
+      <button className="btn" onClick={() => setValue(value + 1)}>
+        click me
+      </button>
+    </div>
+  );
 };
 
 export default UseEffectBasics;
