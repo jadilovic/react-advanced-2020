5e3c414c8abf53fa29b21e5e789041a7d52e5140
jadilovic <adilovic79@yahoo.com>
2021-04-06 16:22:20 +0200

    renderning

--- tutorial/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ tutorial/src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -7,16 +7,31 @@ const UseEffectSecondArgument = () => {
   const getUsers = async () => {
     const response = await fetch(url);
     const usersdata = await response.json();
-    console.log(usersdata);
+    setUsers(usersdata);
+    // console.log(usersdata);
   };
 
   useEffect(() => {
     getUsers();
-  });
+  }, []);
 
   return (
     <div>
       <h3>github users</h3>;
+      <ul className="users">
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user;
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt={login} />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>User Profile</a>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 };
