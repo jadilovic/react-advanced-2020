ffc9a67a07b7ad15adc0314bb2f0dbabafd8c053
jadilovic <adilovic79@yahoo.com>
2021-04-07 13:56:04 +0200

    continued tours

--- 2-tours/src/App.js
+++ 2-tours/src/App.js
@@ -4,9 +4,35 @@ import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
+
 function App() {
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
   const [tours, setTours] = useState([]);
+  let errorText = "";
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (e) {
+      setLoading(false);
+      setError(true);
+      errorText = e.toString();
+      console.log(errorText);
+    }
+    console.log(tours);
+  };
+
+  // use effect
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  // if loading
   if (loading) {
     return (
       <main>
@@ -14,10 +40,18 @@ function App() {
       </main>
     );
   }
+  // if error
+  if (error) {
+    return (
+      <main>
+        <h2>There is following error detected: {errorText}</h2>
+      </main>
+    );
+  }
   // default return
   return (
     <main>
-      <Tours />
+      <Tours tours={tours} />
     </main>
   );
 }
