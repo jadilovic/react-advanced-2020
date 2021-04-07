de4456c5d6454271a6ac46cb28f01079c40a7fba
jadilovic <adilovic79@yahoo.com>
2021-04-07 09:59:23 +0200

    starting tours project

---
+++ 2-tours/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = "https://course-api.com/react-tours-project";
+function App() {
+  const [loading, setLoading] = useState(false);
+  const [tours, setTours] = useState([]);
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  // default return
+  return (
+    <main>
+      <Tours />
+    </main>
+  );
+}
+
+export default App;
