0f8f40616333a7f5cf5cc06437f4f0951fd5a660
jadilovic <adilovic79@yahoo.com>
2021-04-10 19:44:59 +0200

    completed tabs

--- 7-slider/src/App.js
+++ 7-slider/src/App.js
@@ -8,6 +8,24 @@ function App() {
   const [value, setValue] = useState(0);
   console.log(people);
 
+  // check value
+  const checkValue = (indexValue) => {
+    if (indexValue < 0) {
+      return people.length - 1;
+    } else if (indexValue > people.length - 1) {
+      return 0;
+    } else {
+      return indexValue;
+    }
+  };
+
+  useEffect(() => {
+    const slider = setInterval(() => {
+      setValue(checkValue(value + 1));
+    }, 3000);
+    return () => clearInterval(slider);
+  }, [value]);
+
   return (
     <section className="section">
       <div className="title">
@@ -18,8 +36,19 @@ function App() {
       <div className="section-center">
         {people.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
+          // positions
+          let position = "nextSlide";
+          if (personIndex === value) {
+            position = "activeSlide";
+          }
+          if (
+            personIndex === value - 1 ||
+            (value === 0 && personIndex === people.length - 1)
+          ) {
+            position = "lastSlide";
+          }
           return (
-            <article key={id}>
+            <article key={id} className={position}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
@@ -29,10 +58,14 @@ function App() {
           );
         })}
         <button className="prev">
-          <FiChevronLeft></FiChevronLeft>
+          <FiChevronLeft
+            onClick={() => setValue(() => checkValue(value - 1))}
+          ></FiChevronLeft>
         </button>
         <button className="next">
-          <FiChevronRight></FiChevronRight>
+          <FiChevronRight
+            onClick={() => setValue(() => checkValue(value + 1))}
+          ></FiChevronRight>
         </button>
       </div>
     </section>
