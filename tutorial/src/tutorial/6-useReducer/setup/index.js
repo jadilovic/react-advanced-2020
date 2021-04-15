5de3901ff9427e0d4d653223776ad51e0c24bc64
jadilovic <adilovic79@yahoo.com>
2021-04-15 21:54:02 +0200

    started working on prop dirlling

--- tutorial/src/tutorial/6-useReducer/setup/index.js
+++ tutorial/src/tutorial/6-useReducer/setup/index.js
@@ -1,33 +1,41 @@
 import React, { useState, useReducer } from "react";
 import Modal from "./Modal";
 import { data } from "../../../data";
-// reducer function
+import { reducer } from "./reducer";
+
+const defaultState = {
+  people: data,
+  isModalOpen: false,
+  modalContent: "",
+};
 
 const Index = () => {
   const [name, setName] = useState("");
-  const [people, setPeople] = useState(data);
-  const [showModal, setShowModal] = useState(false);
+  const [state, dispatch] = useReducer(reducer, defaultState);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (name) {
-      setShowModal(true);
-      setPeople([
-        ...people,
-        {
-          id: new Date().getTime().toString(),
-          name,
-        },
-      ]);
+      const newItem = { id: new Date().getTime().toString(), name };
+      dispatch({
+        type: "ADD_ITEM",
+        payload: newItem,
+      });
       setName("");
     } else {
-      setShowModal(true);
+      dispatch({ type: "NO_VALUE" });
     }
+  };
+
+  const closeModal = () => {
+    dispatch({ type: "CLOSE_MODAL" });
   };
 
   return (
     <div>
-      {showModal && <Modal />}
+      {state.isModalOpen && (
+        <Modal modalContent={state.modalContent} closeModal={closeModal} />
+      )}
       <form onSubmit={handleSubmit} className="form">
         <div>
           <input
@@ -38,11 +46,17 @@ const Index = () => {
         </div>
         <button type="submit">add</button>
       </form>
-      {people.map((person) => {
+      {state.people.map((person) => {
         const { id, name } = person;
         return (
-          <div key={id}>
+          <div key={id} className="item">
             <h4>{name}</h4>
+            <button
+              type="button"
+              onClick={() => dispatch({ type: "REMOVE_ITEM", payload: id })}
+            >
+              remove
+            </button>
           </div>
         );
       })}
--- tutorial/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
+++ tutorial/src/tutorial/7-prop-drilling/setup/1-prop-drilling.js
@@ -1,10 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 // more components
 // fix - context api, redux (for more complex cases)
 
 const PropDrilling = () => {
-  return <h2>prop drilling</h2>;
+  const [people, setPeople] = useState(data);
+
+  return (
+    <section>
+      <h3>prop drilling</h3>
+      <List people={people} />
+    </section>
+  );
+};
+
+const List = ({ people }) => {
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id} className="list">
+            <h5>{name}</h5>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default PropDrilling;
