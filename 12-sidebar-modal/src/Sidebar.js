6595c45e4a3fca598fe9a5a2cfcc96e56f353106
jadilovic <adilovic79@yahoo.com>
2021-04-17 10:10:30 +0200

    continuing on useContext exercises

--- 12-sidebar-modal/src/Sidebar.js
+++ 12-sidebar-modal/src/Sidebar.js
@@ -25,6 +25,16 @@ const Sidebar = () => {
           );
         })}
       </ul>
+      <ul className="social-icons">
+        {social.map((soc) => {
+          const { id, url, icon } = soc;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
     </aside>
   );
 };
