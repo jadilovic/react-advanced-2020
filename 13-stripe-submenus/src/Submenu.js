f969e38509f575f55ab86d7fd6213481f8f401e3
jadilovic <adilovic79@yahoo.com>
2021-04-18 15:27:42 +0200

    submenu construction

--- 13-stripe-submenus/src/Submenu.js
+++ 13-stripe-submenus/src/Submenu.js
@@ -2,10 +2,23 @@ import React, { useState, useRef, useEffect } from "react";
 import { useGlobalContext } from "./context";
 
 const Submenu = () => {
-  const { isSubmenuOpen } = useGlobalContext();
+  const { isSubmenuOpen, location } = useGlobalContext();
+
+  const container = useRef(null);
+
+  useEffect(() => {
+    const submenu = container.current;
+    const { center, bottom } = location;
+    console.log(center + " " + bottom);
+    submenu.style.left = `${center}px`;
+    submenu.style.top = `${bottom}px`;
+  }, [location]);
 
   return (
-    <article className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}>
+    <article
+      className={`${isSubmenuOpen ? "submenu show" : "submenu"}`}
+      ref={container}
+    >
       submenu
     </article>
   );
