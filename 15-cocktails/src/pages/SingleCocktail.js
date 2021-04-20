fd90fbcbc6886fbd227505a42b82400f8bc7e555
jadilovic <adilovic79@yahoo.com>
2021-04-20 23:54:00 +0200

    almost complete Router project

--- 15-cocktails/src/pages/SingleCocktail.js
+++ 15-cocktails/src/pages/SingleCocktail.js
@@ -1,14 +1,17 @@
-import React from 'react'
-import Loading from '../components/Loading'
-import { useParams, Link } from 'react-router-dom'
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
+import React from "react";
+import Loading from "../components/Loading";
+import { useParams, Link } from "react-router-dom";
+const url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
 const SingleCocktail = () => {
+  const cocktailId = useParams();
+  console.log(cocktailId);
+
   return (
     <div>
       <h2>single cocktail page </h2>
     </div>
-  )
-}
+  );
+};
 
-export default SingleCocktail
+export default SingleCocktail;
